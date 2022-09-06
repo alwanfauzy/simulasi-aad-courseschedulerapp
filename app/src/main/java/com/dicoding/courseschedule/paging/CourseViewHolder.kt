d5968c49a9bd29ec776eab2b6b05a7ed2a483452
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-06 13:37:34 +0700

    Solve TODO 7

--- app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
+++ app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
@@ -1,17 +1,22 @@
 package com.dicoding.courseschedule.paging
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.util.DayName.Companion.getByNumber
 
-class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class CourseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private lateinit var course: Course
     private val timeString = itemView.context.resources.getString(R.string.time_format)
 
     //TODO 7 : Complete ViewHolder to show item
+    private val tvCourse: TextView = itemView.findViewById(R.id.tv_course)
+    private val tvTime: TextView = itemView.findViewById(R.id.tv_time)
+    private val tvLecturer: TextView = itemView.findViewById(R.id.tv_lecturer)
+
     fun bind(course: Course, clickListener: (Course) -> Unit) {
         this.course = course
 
@@ -19,12 +24,12 @@ class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
 
-
+            tvCourse.text = courseName
+            tvTime.text = timeFormat
+            tvLecturer.text = lecturer
         }
 
-        itemView.setOnClickListener {
-            clickListener(course)
-        }
+        itemView.setOnClickListener { clickListener(course) }
     }
 
     fun getCourse(): Course = course
