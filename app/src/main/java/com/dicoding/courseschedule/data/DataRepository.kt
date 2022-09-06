ffbc8518db76a71db0e9fbeb5613a817b10899ce
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-06 12:52:28 +0700

    Solve TODO 4

--- app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
@@ -3,38 +3,39 @@ package com.dicoding.courseschedule.data
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.Config
 import androidx.paging.PagedList
+import androidx.paging.toLiveData
 import com.dicoding.courseschedule.util.QueryType
+import com.dicoding.courseschedule.util.QueryUtil
 import com.dicoding.courseschedule.util.QueryUtil.nearestQuery
 import com.dicoding.courseschedule.util.SortType
 import com.dicoding.courseschedule.util.executeThread
+import java.util.*
 
 //TODO 4 : Implement repository with appropriate dao
 class DataRepository(private val dao: CourseDao) {
 
-    fun getNearestSchedule(queryType: QueryType) : LiveData<Course?> {
-        throw NotImplementedError("needs implementation")
+    fun getNearestSchedule(queryType: QueryType): LiveData<Course?> {
+        val query = QueryUtil.nearestQuery(queryType)
+        return dao.getNearestSchedule(query)
     }
 
     fun getAllCourse(sortType: SortType): LiveData<PagedList<Course>> {
-        throw NotImplementedError("needs implementation")
+        val query = QueryUtil.sortedQuery(sortType)
+        return dao.getAll(query).toLiveData(Config(PAGE_SIZE))
     }
 
-    fun getCourse(id: Int) : LiveData<Course> {
-        throw NotImplementedError("needs implementation")
+    fun getCourse(id: Int): LiveData<Course> = dao.getCourse(id)
+
+    fun getTodaySchedule(): List<Course> {
+        val today = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+        return dao.getTodaySchedule(today)
     }
 
-    fun getTodaySchedule() : List<Course> {
-        throw NotImplementedError("needs implementation")
-    }
+    fun insert(course: Course) = executeThread { dao.insert(course) }
 
-    fun insert(course: Course) = executeThread {
-
-    }
-
-    fun delete(course: Course) = executeThread {
-
-    }
+    fun delete(course: Course) = executeThread { dao.delete(course) }
 
     companion object {
         @Volatile
